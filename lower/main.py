6fceb19821523acd5582ec4d7a41f0d0560d1cd3
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-12-30 14:30:15 -0500

    lower_python
    lower_python

---
+++ lower/main.py
@@ -0,0 +1,9 @@
+# Se crea una variable llamada 'curso' y se le asigna un texto (string)
+curso = "APRENDIENDO PYTHON CON JULIAN"
+
+# Se crea otra variable 'mi_curso'
+# El método .lower() convierte todo el texto a letras minúsculas
+mi_curso = curso.lower()
+
+# Se imprime en pantalla el contenido de la variable 'mi_curso'
+print(mi_curso)
